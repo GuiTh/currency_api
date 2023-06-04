a1d166d257227f26603cd8a1b92277ec32ff110a
Guilherme Carlos <56705014+GuiTh@users.noreply.github.com>
2023-06-04 19:17:52 -0300

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/header"
 import InputAmount from "./components/inputAmount"
 import SelectCountry from "./components/SelectCountry"
 import SwitchCurrency from "./components/SwitchCurrency"
-import { Container, Grid, Button, Typography } from "@mui/material"
+import { Container, Grid, Button, Typography, Box } from "@mui/material"
 import { useContext, useEffect, useState } from "react"
 import { CurrencyContext } from "./context/CurrencyContext"
 import axios from "axios"
@@ -18,22 +18,24 @@ function App() {
     firstAmount,
   } = useContext(CurrencyContext)
 
-  const [resultCurrency, setResultCurrency] = useState(0)
+  const [resultCurrency, setResultCurrency] = useState(1)
   const codeFromCurrency = fromCurrency.split(' ')[1]
   const codeToCurrency = toCurrency.split(' ')[1]
+  
 
   useEffect(() =>{
-    if(firstAmount){
-      axios("https://api.freecurrencyapi.com/v1/latest", {
-        params:{
-          apiKey:'wbsq9SwSt5X5hGbNUzfJ4lxADJxbA4HCXlw0z6tv',
-          base_currency: codeFromCurrency,
-          currencies: codeToCurrency
-        }
-      }).then(res => setResultCurrency(res.data.data[codeToCurrency]))
-      .catch(error => console.log(error))
-    }
-  }, [firstAmount])
+   if(firstAmount){
+    axios('https://api.freecurrencyapi.com/v1/latest',{
+      params: {
+        apikey: import.meta.env.VITE_API_KEY,
+        base_currency: codeFromCurrency,
+        currencies: codeToCurrency
+      }
+    })
+    .then(response => setResultCurrency(response.data.data[codeToCurrency]))
+    .catch(error => console.log(error))
+   }
+  }, [firstAmount, fromCurrency, toCurrency])
 
   const boxStyles = {
     background: '#fdfdfd',
@@ -62,6 +64,12 @@ function App() {
           <SwitchCurrency />
           <SelectCountry value={toCurrency} setValue={setToCurrency} label="To" />
         </Grid>
+        {firstAmount ? (
+          <Box sx={{textAlign:"left", marginTop: '1rem'}}>
+            <Typography>{firstAmount} {fromCurrency} =</Typography>
+            <Typography variant="h5" sx={{marginTop: '5px', fontWeight:'bold'}}>{resultCurrency} {toCurrency} =</Typography>
+            </Box>
+        ): ''}
       </Container>  
     </div>
     )
